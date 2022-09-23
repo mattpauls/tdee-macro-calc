a454c8d9182ae252348aab0ead9e1fa097158f2d
Matt Pauls <mattpauls@gmail.com>
2022-09-23 15:58:08 -0700

    program structure(ish)

---
+++ main.py
@@ -0,0 +1,34 @@
+
+# TDEE Calculation
+# Gather input from user
+    # First/initial input of weight should set "current weight" to be that one.
+    # Input should include date, weight, and calories
+    # To make it easy, it might be helpful to default to the next day for the date, since this is a command line program, currently
+    # All this data needs to be stored somewhere, likely a dictionary in memory to start but would be helpful to have a database or, possibly, a CSV file if we want to be more hacky
+
+# Run calculations
+    # Once the input is done, we can take all the input and get an average of calories
+    # Ideally, we'd have two weeks worth of data. Perhaps add some disclaimers if we don't have enough data points to make a good call
+    # Perhaps if the dates are too far apart, we can suggest starting over
+
+# Input
+    # Current weight
+    # Current TDEE
+        # Calculated off of TDEE calculator
+    # (optional, maybe just default to .8) Per-lb protein intake (.8-1.0)
+    # (optional, maybe just default to .4) Per-lb fat intake (.4-.5)
+
+# Output
+    # Current TDEE (average of current daily calories)
+    # Current average weight
+    # Target daily calorie deficit (3.2kcal/lb)
+    # Target daily calorie intake (Current TDEE minus target daily calorie deficit)
+    # Macros - first hit the calorie goal, then protein, then fat, then fill rest with carbs
+        # Protein 4 cal per gram, .8-1.0g/lb for building muscle, 120g-160g/day is a good place to start
+            # Output: how many grams of protein you should eat (and as an FYI, how many calories that is)
+        # Fat 9 cal per gram, minimum .3g/lb
+            # Output: how many grams of fat you should eat (and as an FYI, how many calories that is)
+        # Carbs 4 cal per gram Essentially, take the calories eaten in protein and fat, subtract from your goal calories per day, and the rest is what you can eat in carbs. However, carbs/fat really doesn't matter, what is most important is protein.
+            # Output: how many grams of carbs you should eat (and as an FYI, how many calories that is)
+
+
