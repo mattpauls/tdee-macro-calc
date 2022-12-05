be0d6b7edd801cb39cbbf09ff15c9240e7075a3a
Matt Pauls <mattpauls@gmail.com>
2022-12-04 17:49:35 -0800

    add parameters

--- tests/test_check_valid_date.py
+++ tests/test_check_valid_date.py
@@ -4,6 +4,10 @@ from tdee_macro_calc.main import check_valid_date
 @pytest.mark.parametrize("test_input,expected", [
     ("5/21/1989", True),
     (5.21, False),
+    (182.3, False),
+    ("182.3", False),
+    (1823, False),
+    ("1823", False),
     ("5/21", False)
 ])
 def test_check_valid_date(test_input, expected):
