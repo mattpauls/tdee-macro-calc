2e9a300fb886ca303f83368a6d99e253e13f7d3d
Matt Pauls <mattpauls@gmail.com>
2022-12-04 18:01:39 -0800

    add parameter

--- tests/test_convert_date.py
+++ tests/test_convert_date.py
@@ -6,6 +6,7 @@ from tdee_macro_calc.main import convert_date
     #TODO add more inputs, takes datetime object and converts to string formatted as MM/DD/YYYY
     # if is already a string, returns the string
     ("5/21/89", "5/21/89"),
+    ("string", "string"),
     (datetime.date(2022, 10, 5), "10/05/2022"),
     (datetime.date(2019, 5, 5), "05/05/2019")
 ])
